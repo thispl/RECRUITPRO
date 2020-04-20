4ea838919543cbb14b9e9743e8e87793b1ac0a02
Sharmila H <sharmiviji1997@gmail.com>
2020-04-20 09:51:34 +0530

    final push

---
+++ apps/recruitpro/recruitpro/recruitpro/doctype/job_description/job_description_dashboard.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+	return {
+		'fieldname': 'job_description',
+		'transactions': [
+			{
+				'label': _('Activity'),
+				'items': ['Timesheet']
+			},
+			{
+				'label': _('Accounting'),
+				'items': ['Expense Claim']
+			}
+		]
+	}
--- apps/recruitpro/recruitpro/recruitpro/doctype/lead/lead.py
+++ apps/recruitpro/recruitpro/recruitpro/doctype/lead/lead.py
@@ -8,3 +8,20 @@ from frappe.model.document import Document
 
 class Lead(Document):
 	pass
+# @frappe.whitelist()
+
+# def make_customer(source_name, target_doc=None):
+# 	return _make_project(source_name, target_doc)
+
+# def _make_customer(source_name, target_doc=None, ignore_permissions=False):	
+# 	doclist = get_mapped_doc("Lead", source_name,
+# 		{"Lead": {
+# 			"doctype": "Customer",
+# 			"field_map": {
+# 				"lead": "from_lead"
+# 				# "contact_no": "phone_1",
+# 				# "fax": "fax_1"
+# 			}
+# 		}}, ignore_permissions=ignore_permissions)
+
+# 	return doclist
